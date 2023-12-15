60f9e14b3b865e4e1624d50e6236793c5e163697
Александр <svinarev_aleksander_andreevich@mail.ru>
2023-12-16 00:17:58 +0700

    реализовал карту стен и её чтение из потока ввода (повторная отправка)

---
+++ My_Nedo_Pakman/Walls_map.cpp
@@ -0,0 +1,21 @@
+#include "Walls_map.h"
+
+void Walls_map::read_walls(std::istream& input) {
+	unsigned short horisontal, vertical;
+	input >> horisontal >> vertical;
+	walls.fill(false);
+	for (unsigned short wall_num = 0; wall_num < horisontal; wall_num++) {
+		unsigned short start_x, finish_x, y;
+		input >> start_x >> y >> finish_x >> y;
+		for (unsigned short x = start_x; x <= finish_x; x++) {
+			walls[y * playing_field_width + x] = true;
+		}
+	}
+	for (unsigned short wall_num = 0; wall_num < horisontal; wall_num++) {
+		unsigned short x, start_y, finish_y;
+		input >> x >> start_y >> x >> finish_y;
+		for (unsigned short y = start_y; y <= finish_y; y++) {
+			walls[y * playing_field_width + x] = true;
+		}
+	}
+}
--- My_Nedo_Pakman/Walls_map.h
+++ My_Nedo_Pakman/Walls_map.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <array>
+#include <istream>
+
+const size_t playing_field_height = 101, playing_field_width = 185;
+
+class Walls_map {
+	std::array<bool, playing_field_height* playing_field_width> walls;
+public:
+	void read_walls(std::istream& input);
+	bool wall_at_point(unsigned short x, unsigned short y) {
+		return walls[y * playing_field_width + x];
+	}
+};
